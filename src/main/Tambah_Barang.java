8281a00974aa5acc518075fb9d89ad07708cdce4
amarullah_rahman <amarullah_rahman@users.noreply.github.com>
2020-05-19 10:39:19 +0700

    la finale

--- src/main/Tambah_Barang.java
+++ src/main/Tambah_Barang.java
@@ -231,23 +231,4 @@ public class Tambah_Barang extends javax.swing.JFrame {
         }
     }                                      
 
-    
-    
-    public static void main(String args[]) {
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Tambah_Barang.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        
-        // Display form
-        java.awt.EventQueue.invokeLater(() -> {
-            new Tambah_Barang().setVisible(true);
-        });
-    }
 }
